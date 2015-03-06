1ddc495a754a4ef277c4443baf5854f5bf598866
Brian Gaydon <bwgaydon@gmail.com>
2015-03-05 19:45:56 -0700

    changed 1 to singleton

--- Problem1/ProcessQueue.java
+++ Problem1/ProcessQueue.java
@@ -3,16 +3,27 @@
 import java.util.*;
 
 public class ProcessQueue {
+	
+	//singleton queue and ProcessQueue object
+	private static ProcessQueue PQ;
 	private static LinkedList queue;
 	
-	public ProcessQueue(LinkedList q) {
-		queue = q;
+	private ProcessQueue() {
+		queue = new LinkedList();
+	}
+	
+	public static ProcessQueue getQueue() {
+		if(queue == null) {
+			PQ = new ProcessQueue();
+			System.out.println("Singleton Queue created");
+		}
+		return PQ;
 	}
 	
 	public void add(Submission s) {
 		queue.addFirst(s);
 		System.out.println("Added " + queue.getFirst());
-		System.out.println(queue.size() + " elements in the queue");
+		System.out.println(queue.size() + " Submission(s) in the queue");
 	}
 	
 	public void process() {
@@ -20,8 +31,8 @@ public class ProcessQueue {
 		
 		if (!queue.isEmpty()) {
 			System.out.println("Processed item: " + queue.getLast());
-			System.out.println("There are " + queue.size() + " Submission(s) in the queue.");
 			queue.removeLast();
+			System.out.println("There are " + queue.size() + " Submission(s) in the queue.");
 		}
 		else System.out.println("There are no more Submissions in the queue.");
 	}
--- Problem1/Tester.java
+++ Problem1/Tester.java
@@ -3,27 +3,26 @@ import java.io.*;
 
 public class Tester{
     public static void main(String[] args){
-    	//Grader object provides queue for modification by ProcessQueue
-    	Grader grader = new Grader();
     	
-    	//ProcessQueues to modify objects in queue
-    	ProcessQueue pq1 = grader.processQFactory();
-    	ProcessQueue pq2 = grader.processQFactory();
-    	ProcessQueue pq3 = grader.processQFactory();
-    	ProcessQueue pq4 = grader.processQFactory();
-    	ProcessQueue pq5 = grader.processQFactory();
+    	ProcessQueue PQ = ProcessQueue.getQueue();
     	
-    	//three submission objects added
     	Submission s1 = new Submission();
     	Submission s2 = new Submission();
     	Submission s3 = new Submission();
+    	Submission s4 = new Submission();
+    	Submission s5 = new Submission();
     	
-    	//test add/remove
-    	pq1.add(s1);
-    	pq2.add(s2);
-    	pq3.add(s3);
-    	pq4.process();
-    	pq5.process();
-    	pq1.process();
+    	PQ.add(s1);
+    	PQ.add(s2);
+    	PQ.add(s3);
+    	PQ.process();
+    	PQ.add(s4);
+    	PQ.add(s5);
+    	PQ.process();
+    	PQ.process();
+    	PQ.process();
+    	PQ.process();
+    	PQ.process();
+    	
     }
 }
